cbc22a8c5a82bd7ac04ba5c9b1e150d80cdd8ffc
Alexsandro Tirloni <tirloni.rs@gmail.com>
2022-09-16 22:54:46 -0300

    removendo dados

--- index.js
+++ index.js
@@ -44,7 +44,22 @@ app.post('/users/create', async (req, res) => {
     res.redirect('/')
 })
 
-// listar todos usuários
+app.get('/users/:id',async (req, res) => {
+    const id = req.params.id
+
+    const user = await User.findOne({ raw: true, where: {id: id}})
+
+    res.render('userview', { user } )
+})
+
+app.post('/users/delete/:id', async (req, res) =>{
+    const id = req.params.id
+
+    await User.destroy({ where: { id: id } })
+
+    res.redirect('/')
+})
+
 app.get('/', async (req, res) => {
     const users = await User.findAll({raw: true})
     console.log(users)
